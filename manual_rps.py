63700ee319ed6fdd1bc985d4b320b34444eb5eff
Keshav P <fedkeshav@gmail.com>
2023-06-06 19:06:00 +0100

    Updated manual py file

--- manual_rps.py
+++ manual_rps.py
@@ -13,15 +13,15 @@ def get_winner(computer_choice, user_choice):
     if (computer_choice == 'Rock' and user_choice == 'Scissors') or \
         (computer_choice == 'Scissors' and user_choice == 'Paper') or \
         (computer_choice == 'Paper' and user_choice == 'Rock'):
-            print('YOU LOST')
+            print('You lost')
     elif computer_choice == user_choice:
-         print('IT IS A TIE')
+         print('It is a tie!')
     else:
-         print('YOU WIN')
+         print('You win')
 
 #%%
 def play():
-     computer_choice = get_computer_choice(choice_list)
+     computer_choice = get_computer_choice()
      user_choice = get_user_choice()
      get_winner(computer_choice,user_choice)
      print(f'Computer chose {computer_choice} and User chose {user_choice}')
